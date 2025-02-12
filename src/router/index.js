0fddaed9484d1dd954226ed99f8c32e9123dad0c
Mel-aine <melaineevans7@gmail.com>
2025-02-12 18:06:40 +0100

    new commit

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/fr/categorie/:categoryId',
+      name: 'DetailService',
+      component: () => import('../components/services/DetailServices.vue'),
+    },
   ],
 })
 
