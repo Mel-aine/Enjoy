ff85ce211c5cbd13b13a47e5b969074a4dc2449b
Jb2s <toukamjeanbernard@gmail.com>
2025-02-13 16:27:20 +0100

    nice home interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,12 @@ export default {
       },
       colors:{
         customBlue: '#7B2CBF',
+        customBlueVariant: '#8F3BCC',
         customRed: '#FF5400',
         customNeutreColor: '#FCEFE3',
         customDark: '#171616',
+        customDarkVariant1: '#3e3e3e',
+        customDarkVariant2: '#737373',  
         customWhite: '#fff9f9'
       },
     },
