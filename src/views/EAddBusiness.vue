8ca4810ec19ef5f9044565f5b1df292b111ea12b
Mel-aine <melaineevans7@gmail.com>
2025-02-24 13:25:53 +0100

    commit

--- src/views/EAddBusiness.vue
+++ src/views/EAddBusiness.vue
@@ -1,8 +1,7 @@
 <template>
 <div class="flex items-center justify-center min-h-screen py-10 px-4">
   <div class="bg-white w-full max-w-md sm:max-w-lg md:max-w-3xl p-6 md:p-8 shadow-2xl rounded-2xl">
-    <h2 class="text-xl font-semibold text-gray-950 text-center mb-6">{{ $t('form') }}</h2>
-    <form @submit.prevent="addBusiness" >
+    <h2 class="text-3xl font-semibold text-customBlue text-center mb-6">{{ $t('form') }}</h2>
       <div class="space-y-4">
         <FloatingInput :lb="$t('adresse_1')" :isRequired="true" v-model="Info.address1" />
         <FloatingInput :lb="$t('adresse_2')" v-model="Info.address2"/>
@@ -10,8 +9,8 @@
           <FloatingInput class="w-full" :lb="$t('ville')" v-model="Info.city" />
           <FloatingInput class="w-full" :lb="$t('code')" v-model="Info.code" />
         </div>
-        <FloatingInput :lb="$t('telephone')" v-model="Info.phone"/>
-        <FloatingInput :lb="$t('adresse_web')" v-model="Info.web_address"/>
+        <FloatingInput :inputType="phone" :lb="$t('telephone')" v-model="Info.phone"/>
+        <FloatingInput :inputType="email" :lb="$t('adresse_web')" v-model="Info.web_address"/>
 
         <div>
           <span class="block text-sm text-gray-950 font-semibold mb-1">{{ $t('horaires') }}</span>
@@ -25,12 +24,11 @@
         </div>
 
         <FloatingInput :lb="$t('category')" v-model="Info.category"/>
-        <FloatingInput :lb="$t('adress_email')" v-model="Info.email"/>
-          <Button variant="danger" class="py-2.5"> {{ $t('ajouter_commerce') }}</Button>
+        <FloatingInput :inputType="email" :lb="$t('adress_email')" v-model="Info.email"/>
+          <Button @click="addBusiness" variant="danger" class="py-2.5"> {{ $t('ajouter_commerce') }}</Button>
 
           <!-- <button class="w-full sm:w-1/4 bg-customRed text-white py-3 rounded-lg text-lg font-medium hover:bg-customBlue transition">Ajouter Commerce</button> -->
       </div>
-    </form>
   </div>
 </div>
 
@@ -86,6 +84,17 @@ const addSchedule = () =>{
 }
 const addBusiness = () =>{
   Business.value.push(Info.value)
+  Info.value = ({
+  address1 : '',
+  address2 : '',
+  city : '',
+  phone : '',
+  web_address : '',
+  category : '',
+  email :'',
+  code : '',
+  schedules : [],
+})
   console.log("00000",Business.value)
 }
 </script>
