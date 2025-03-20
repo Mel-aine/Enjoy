7faa8e56a20b14f328fcde56fc461a0206a92ba1
Jb2s <toukamjeanbernard@gmail.com>
2025-03-20 09:35:14 +0100

    remove .env file

--- src/components/CustomModal.vue
+++ src/components/CustomModal.vue
@@ -4,6 +4,7 @@
       class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
       @click="closeModal"
     >
+    
       <div class="bg-white rounded-lg p-6 max-w-md w-full relative" @click.stop>
         <span class="absolute top-2 right-2 text-gray-500 cursor-pointer text-2xl" @click="closeModal">
           &times;
